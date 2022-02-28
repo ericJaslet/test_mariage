1fbfdfd6bd2d8ad6aa61c3d5c5ad76675bc3631c
EJaslet <contact@ericjaslet.fr>
2022-02-28 17:42:27 +0100

    up

--- src/DataFixtures/ThemeFixtures.php
+++ src/DataFixtures/ThemeFixtures.php
@@ -6,7 +6,7 @@ use App\Entity\Theme;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class ThemeFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
---
+++ tests/Func/AbstractEndPoint.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Tests\Func;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+abstract class AbstractEndPoint extends WebTestCase
+{
+    
+}
